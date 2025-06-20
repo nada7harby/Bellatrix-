efad2ecc4d1603689b021ec08b2a163a453cc3ff
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-06-21 01:14:23 +0300

    all

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600 mb-8">
+        مرحباً بكم في Bellatrix
+      </h1>
+    </>
+  );
+}
+
+export default App;
