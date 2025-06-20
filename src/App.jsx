325cf8ac2e8c99ecbe70e2f7ab6f98bdba0e7083
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-06-21 01:01:25 +0300

    remove css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1 className="text-4xl font-bold text-center text-blue-600 mb-8"> nada </h1>
+      <h1 className="text-4xl font-bold text-blue-600 mb-8">
+        مرحباً بكم في Bellatrix
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
